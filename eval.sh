efc25454364499f728092d1733477e8414e0710b
june515 <pj00515@naver.com>
2024-06-23 16:10:10 +0900

    a

---
+++ eval.sh
@@ -0,0 +1,5 @@
+python test.py --model_path checkpoints/june/roberta-base-ann5
+python test.py --model_path checkpoints/june/roberta-base-bm255
+python test.py --model_path checkpoints/june/roberta-base-mixed5
+python test.py --model_path checkpoints/june/roberta-base-random5
+python test.py --model_path checkpoints/june/roberta-base-mixed5ST
